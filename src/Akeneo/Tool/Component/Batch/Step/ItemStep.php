a4cf041a2ea7e26c17b8d98c83df2cce0069c76c
Marin Fazio <marin.fazio@akeneo.com>
2023-06-22 11:09:37 +0200

    RAB-1446: remove test instructions

--- src/Akeneo/Tool/Component/Batch/Step/ItemStep.php
+++ src/Akeneo/Tool/Component/Batch/Step/ItemStep.php
@@ -90,9 +90,7 @@ class ItemStep extends AbstractStep implements TrackableStepInterface, LoggerAwa
             $stepExecution->setTotalItems($this->getCountFromTrackableItemReader());
         }
 
-        $this->batchSize = 1;
         while (true) {
-            usleep(100000);
             try {
                 $readItem = $this->reader->read();
                 if (null === $readItem) {
